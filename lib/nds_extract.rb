fa14040c96bfc19a28632e860e20469be399afff
Pavel Beletsky <pavel.beletsky@live.com>
2019-10-18 19:10:14 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -14,6 +14,11 @@ end
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
+  
   dir_name = directors_database[director_data][:name]
   movies = directors_database[director_data][:movies]
+  movie_index = 0
+  total_profit = 0 
+  while movie_index < movies.length do
+    total_profit += movies[movie_index][]
 end
